9e2a4f78d2eaa713d73db75c6ae5b46e69d42200
drissakande <114373066+Kandedriva@users.noreply.github.com>
2024-07-22 18:46:21 -0400

    Use Bcrypt to hash user's password.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express  from "express";
 import bodyParser from "body-parser";
+import bcrypt from "bcrypt"
 import env from "dotenv";
 import cors from "cors";
 import pg from "pg";
 
+// const bcrypt = bcrypt
 const app = express();
 const port = 5001
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
 env.config();
 app.use(bodyParser.json())
 app.use(cors())
@@ -24,17 +30,39 @@ app.get("/", (req, res)=>{
   res.send("<h1>Hello from the Server</h1>")
 })
 
+bcrypt.genSalt(saltRounds, function(err, salt){
+  bcrypt.hash(myPlaintextPassword, salt, function(err, hash){
+     
+  }) 
+})
 app.post("/registration", async(req, res)=>{
-  const {username, email, password} = req.body
-  console.log(username, email, password)
+  const {username, email, password} = req.body;
+  // console.log(username, email, password)
 
-try {
-  const newUser = await db.query("INSERT INTO networking_users (user_name, email, user_password) VALUES($1, $2, $3)", [username, email, password])
-  console.log(newUser)
-} catch (error) {
+ try {
+  const userExist = await db.query("SELECT * FROM networking_users WHERE email = $1", [email])
+  if(userExist.rows.length>0){
+    console.log("email already exist, try to log in.")
+  }else{
+    bcrypt.hash(password, saltRounds, async function(err, hash){
+     if(err){
+      console.log(err)
+     }else{
+      const newUser = await db.query("INSERT INTO networking_users (user_name, email, user_password) VALUES($1, $2, $3)", [username, email, hash])
+      console.log(newUser)
+     }
+    }) 
+  }
+ } catch (error) {
   console.log(error)
-}  
-res.json({message: "Hello from the Server"})
+ }
+// try {
+//   const newUser = await db.query("INSERT INTO networking_users (user_name, email, user_password) VALUES($1, $2, $3)", [username, email, password])
+//   console.log(newUser)
+// } catch (error) {
+//   console.log(error)
+// }  
+// res.json({message: "Hello from the Server"})
 })
 
 
